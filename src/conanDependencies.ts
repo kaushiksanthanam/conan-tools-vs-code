d6fae3297d55a8682afa724dc103ad2d32985a2f
SANTHANAM, KAUSHIK (K-Atlanta,ex-1) <kaushik.santhanam@keysight.com>
2018-08-27 18:43:52 -0400

    added install and build steps

--- src/conanDependencies.ts
+++ src/conanDependencies.ts
@@ -1,16 +1,15 @@
 import * as vscode from 'vscode';
 import * as path from 'path';
 import * as fs from 'fs';
-import exec from 'child_process';
-import * from 'lazy';
+import { execSync, exec } from 'child_process';
 
 export class ConanDependenciesProvider implements vscode.TreeDataProvider<ConanDependency>
 {
     onDidChangeTreeData?: vscode.Event<ConanDependency | null | undefined> | undefined;
-    private conanFileFromPython;
+    
 
     getTreeItem(element: ConanDependency): vscode.TreeItem | Thenable<vscode.TreeItem> {
-        throw new Error("Method not implemented.");
+        return element;
     }
 
     getChildren(element?: ConanDependency | undefined): vscode.ProviderResult<ConanDependency[]> {
@@ -18,45 +17,76 @@ export class ConanDependenciesProvider implements vscode.TreeDataProvider<ConanD
     }
 
     constructor(private workspace: string | undefined) {
-        this.getConanDependencies('~/Desktop/HackKey/conan-tools-vs-code/conanfile.py')
+       // 1. run conan info command
+       // 2. collect the results into txt file
+       // 3. Read and parse the text file
+       // 4. Create the Conan Dependencies Tree Items
+       let command = 'conan info ' + workspace + '/ --g=' + workspace + '/.conan_tools/conanInfo.dot';
+       exec(command, (err, stdout, stderr) => {
+           if(err){
+               vscode.window.showErrorMessage("Failed to grab conan information. " + stderr.toString());
+           }
+           else{
+               // Read from conanInfo.dot file
+               fs.readFile(workspace + "/.conan_tools/conanInfo.dot", (err, data) => {
+                   if(err){
+                       vscode.window.showErrorMessage("Could not read conanInfo.dot");
+                   }
+                   else{
+                       let fileContents = data.toString();
+                       let lines = fileContents.split('\n');
+                       
+                       lines.forEach(line => {
+                            let split = line.split("->");
+                            if(split.length > 0) {
+                                let parent = split[0];
+                                let children = split[1];
+                                vscode.window.showInformationMessage(parent.toString());
+                            }
+                       });
+                   }
+               });
+           }
+       });
+       
     }
 
-    private getConanDependencies(conanFilePath: string): ConanDependency[] {
-        exec('conan info .  -j > tmpConanOutput.txt', (error, stdout, stderr) => {
-            console.log('stdout: ' + stdout);
-            console.log('stderr: ' + stderr);
-            if (error !== null) {
-                console.log('exec error: ' + error);
-            }
-        });
-        if (fs.exists('tmpConanOutput.txt', () => {
-            //Readin txt file:
-            const input = fs.createReadStream('tmpConanOutput.txt');
-            let ret = Array<ConanDependency>();
-            // TODO:
-            // Read in input line by line
-            // Line starts from \n then skip
-            /*
-            while (inputLine.length() >0 ) {
-                if(inputLine.startWith('\n)) {
-                    skipThisLine();
-                }
-                if (inputLine.firstWord === 'Downloading') {
-                    skipThisLine();
-                } 
-                if (inputLine.firstWord === 'Version') {
-                    skipThisLine(); //Because it's not actual part.
-                }
-                versionLibInfo = inputLine.split('/');
-                // Please match the dependency format.
-                libItem = new ConanDependency(versionLibInfo[0],versionLibInfo[1],versionLibInfo[2]);
-                ret.push(libItem);
-            }
-            */
+    private getConanDependencies(conanFilePath: string): void {
+        // exec('conan info .  -j > ./conan-tools/conanInstallInfo.txt', (error, stdout, stderr) => {
+        //     console.log('stdout: ' + stdout);
+        //     console.log('stderr: ' + stderr);
+        //     if (error !== null) {
+        //         console.log('exec error: ' + error);
+        //     }
+        // });
+        // if (fs.exists('tmpConanOutput.txt', () => {
+        //     //Readin txt file:
+        //     const input = fs.createReadStream('tmpConanOutput.txt');
+        //     let ret = Array<ConanDependency>();
+        //     // TODO:
+        //     // Read in input line by line
+        //     // Line starts from \n then skip
+        //     /*
+        //     while (inputLine.length() >0 ) {
+        //         if(inputLine.startWith('\n)) {
+        //             skipThisLine();
+        //         }
+        //         if (inputLine.firstWord === 'Downloading') {
+        //             skipThisLine();
+        //         } 
+        //         if (inputLine.firstWord === 'Version') {
+        //             skipThisLine(); //Because it's not actual part.
+        //         }
+        //         versionLibInfo = inputLine.split('/');
+        //         // Please match the dependency format.
+        //         libItem = new ConanDependency(versionLibInfo[0],versionLibInfo[1],versionLibInfo[2]);
+        //         ret.push(libItem);
+        //     }
+        //     */
             
-        })) {
+        // })) {
 
-        }
+        // }
     }
 
 }
--- src/extension.ts
+++ src/extension.ts
@@ -2,31 +2,96 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
+import * as fs from 'fs';
+import * as path from 'path';
+
+
 import { ConanDependenciesProvider } from './conanDependencies';
+import { execSync, exec } from 'child_process';
+import { ProgressLocation } from 'vscode';
+
+let rootPath:string | undefined;
+let buildPath:string;
+let conan_tmpFolder:string;
 
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
 
-    const rootPath = vscode.workspace.rootPath;
+    rootPath = vscode.workspace.rootPath;
+    buildPath = rootPath + '/build';
+    conan_tmpFolder = rootPath + '/.conan_tools';
+
+    if(!fs.existsSync(conan_tmpFolder)){
+        fs.mkdirSync(conan_tmpFolder);
+    }
     const conanDependencyProvider = new ConanDependenciesProvider(rootPath);
 
-    vscode.window.registerTreeDataProvider('conan.install', conanDependencyProvider);
-    // The command has been defined in the package.json file
-    // Now provide the implementation of the command with  registerCommand
-    // The commandId parameter must match the command field in package.json
-    let disposable = vscode.commands.registerCommand('extension.sayHello', () => {
-        // The code you place here will be executed every time your command is executed
-
-        // Display a message box to the user
-        vscode.window.showInformationMessage('Hello World!');
-    });
-
     vscode.commands.registerCommand('conan.install', () => {
-        vscode.window.showInformationMessage('Conan install completed');
+        
+        vscode.window.withProgress({
+            location:ProgressLocation.Notification,
+            title: "Conan Tools"
+        }, (progress, token) => {
+            let p = new Promise(resolve => {
+                progress.report({message: "Installing" });
+                setImmediate(() => {
+                    
+                    // Create build directory if non existent
+                    if(!fs.existsSync(buildPath))
+                    {
+                        fs.mkdirSync(vscode.workspace.rootPath + '/build/');
+                    }
+
+                    exec('conan install ..', {cwd: buildPath}, (err, stdout, stderr) => {
+                        if(err)
+                        {
+                            progress.report({message: "Install Failed. " + stderr.toString()});
+                            vscode.window.showErrorMessage(stderr.toString());
+                        } else {
+                            vscode.window.showInformationMessage('Conan Tools: Installed conan dependencies');
+                        }
+                        resolve();
+                    });
+                });
+            });
+            return p;
+        });
+        
+
     });
-    
-    context.subscriptions.push(disposable);
+
+    vscode.commands.registerCommand('conan.build', () => {
+
+        vscode.window.withProgress({
+            location:ProgressLocation.Notification,
+            title: "Conan Tools"    
+        }, (progress, token) => {
+            progress.report({message: "Building" });
+            let p = new Promise(resolve => {
+                setImmediate(() => {
+
+                    if(!fs.existsSync(buildPath))
+                    {
+                        fs.mkdirSync(vscode.workspace.rootPath + '/build/');
+                    }
+                    
+                    exec('conan build ..', {cwd: buildPath}, (err, stdout, stderr) => {
+                        if(err){
+                            progress.report({message: "Build Failed. " + err.stack});
+                            vscode.window.showErrorMessage(err.stack ? err.stack.toString(): "Build Failed");
+                        } else {
+                            progress.report({message: "Build completed"});
+                            vscode.window.showInformationMessage("Conan Tools: Build completed");
+                        }
+                        resolve();
+                    });
+
+                });
+            });
+            return p;
+        });
+    });
 }
 
 // this method is called when your extension is deactivated
